eadf1fba2cc44f0e9e4681b61f8acadc3364e3f8
Tejas Nair <tejasvnair@gmail.com>
2022-02-16 16:46:58 +0900

    Valid Binary Search Tree: LC 98

---
+++ valid_binary_search_tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root, lbound, rbound):
+            # Node out of acceptable range
+            if root.val >= rbound or root.val <= lbound:
+                return False
+            
+            # check if its leaf node
+            if root.left is None and root.right is None:
+                return True
+            elif root.left is None:
+                return dfs(root.right, root.val, rbound)
+            elif root.right is None:
+                return dfs(root.left, lbound, root.val)
+            else:
+                return dfs(root.left, lbound, root.val) and dfs(root.right, root.val, rbound)
+                
+        return dfs(root, float(-inf), float(inf))
